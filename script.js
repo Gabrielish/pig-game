edb37429ac2717e4961a4130197563a411c41879
Gabrielish <gabriel_bundea@hotmail.com>
2022-02-01 01:24:25 +0200

    Resetting the Game

--- script.js
+++ script.js
@@ -20,17 +20,29 @@ const switchPlayer = function () {
   player1El.classList.toggle('player--active')
 }
 
+let scores, currentScore, activePlayer, playing
+
 // Starting conditions
-score0El.textContent = 0
-score1El.textContent = 0
-diceEl.classList.add('hidden')
 
-const scores = [0, 0]
+const init = function () {
+  scores = [0, 0]
+  currentScore = 0
+  activePlayer = 0
+  playing = true
 
-let currentScore = 0
-let activePlayer = 0
-let playing = true
+  score0El.textContent = 0
+  score1El.textContent = 0
+  current0El.textContent = 0
+  current1El.textContent = 0
 
+  diceEl.classList.add('hidden')
+  player0El.classList.remove('player--winner')
+  player1El.classList.remove('player--winner')
+  player0El.classList.add('player--active')
+  player1El.classList.remove('player--active')
+}
+
+init()
 // Rolling dice functionality
 
 btnRoll.addEventListener('click', function () {
@@ -64,7 +76,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer]
     // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // Finish the game
       playing = false
       diceEl.classList.add('hidden')
@@ -79,3 +91,6 @@ btnHold.addEventListener('click', function () {
     }
   }
 })
+
+// Resetting the Game
+btnNew.addEventListener('click', init)
